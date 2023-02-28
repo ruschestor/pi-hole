273147a9d341171ae8dc4e91f7921b52eba88992
RD WebDesign <github@rdwebdesign.com.br>
2023-02-28 15:55:02 -0300

    Search for ABP entries only if they exist in gravity.db
    Signed-off-by: RD WebDesign <github@rdwebdesign.com.br>

--- advanced/Scripts/query.sh
+++ advanced/Scripts/query.sh
@@ -99,29 +99,38 @@ scanDatabaseTable() {
     table="${2}"
     list_type="${3:-}"
 
-    # Create search string for ABP entries
-    local abpentry="${domain}" searchstr
-
-    searchstr="'||${abpentry}^'"
-    while [ "${abpentry}" != "${abpentry/./}" ]
-    do
-        abpentry=$(echo "${abpentry}" | cut -f 2- -d '.')
-        searchstr=$(echo "$searchstr, '||${abpentry}^'")
-    done
-
     # As underscores are legitimate parts of domains, we escape them when using the LIKE operator.
     # Underscores are SQLite wildcards matching exactly one character. We obviously want to suppress this
     # behavior. The "ESCAPE '\'" clause specifies that an underscore preceded by an '\' should be matched
     # as a literal underscore character. We pretreat the $domain variable accordingly to escape underscores.
     if [[ "${table}" == "gravity" ]]; then
+        local searchstr
+
+        # Are there ABP entries on gravity?
+        local abpquerystr="SELECT domain FROM gravity WHERE domain LIKE '||%^' LIMIT 1"
+        local abpfound="$(pihole-FTL sqlite3 "${gravityDBfile}" "${abpquerystr}")" 2> /dev/null
+
+        # Create search string for ABP entries only if needed
+        if [ -n "${abpfound}" ]; then
+            local abpentry="${domain}"
+
+            searchstr="'||${abpentry}^'"
+            while [ "${abpentry}" != "${abpentry/./}" ]
+            do
+                abpentry=$(echo "${abpentry}" | cut -f 2- -d '.')
+                searchstr=$(echo "$searchstr, '||${abpentry}^'")
+            done
+            searchstr=$(echo "domain IN (${searchstr}) OR ")
+        fi
+
         case "${exact}" in
             "exact" ) querystr="SELECT gravity.domain,adlist.address,adlist.enabled FROM gravity LEFT JOIN adlist ON adlist.id = gravity.adlist_id WHERE domain = '${domain}'";;
-            *       ) querystr="SELECT gravity.domain,adlist.address,adlist.enabled FROM gravity LEFT JOIN adlist ON adlist.id = gravity.adlist_id WHERE (domain IN (${searchstr}) OR domain LIKE '%${domain//_/\\_}%' ESCAPE '\\')";;
+            *       ) querystr="SELECT gravity.domain,adlist.address,adlist.enabled FROM gravity LEFT JOIN adlist ON adlist.id = gravity.adlist_id WHERE (${searchstr} domain LIKE '%${domain//_/\\_}%' ESCAPE '\\')";;
         esac
     else
         case "${exact}" in
             "exact" ) querystr="SELECT domain,enabled FROM domainlist WHERE type = '${list_type}' AND domain = '${domain}'";;
-            *       ) querystr="SELECT domain,enabled FROM domainlist WHERE type = '${list_type}' AND (domain IN (${searchstr}) OR domain LIKE '%${domain//_/\\_}%' ESCAPE '\\')";;
+            *       ) querystr="SELECT domain,enabled FROM domainlist WHERE type = '${list_type}' AND domain LIKE '%${domain//_/\\_}%' ESCAPE '\\'";;
         esac
     fi
 
